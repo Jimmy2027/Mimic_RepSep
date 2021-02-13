c970d70ad58baf4adf9449b5e9addcc52f537340
klugh <klugh@student.ethz.ch>
2021-02-13 11:33:58 +0100

    added test_vae_gen.py

---
+++ prepare/test_vae_gen.py
@@ -0,0 +1,46 @@
+# HK, 13.02.21
+import json
+import os
+from pathlib import Path
+
+import torch
+from mimic.dataio.MimicDataset import Mimic
+from mimic.evaluation.eval_metrics.coherence import test_generation, flatten_cond_gen_values
+from mimic.utils.experiment import MimicExperiment
+from mimic.utils.filehandling import set_paths
+
+from logger.logger import log
+from utils import Dummylogger
+from utils import get_config
+
+config = get_config()
+experiment_dir = config['experiment_dir']
+experiment_path = Path(os.getcwd()) / f'data/vae_model/{experiment_dir}'
+flags_path = experiment_path / 'flags.rar'
+FLAGS = torch.load(flags_path)
+FLAGS.save_figure = False
+FLAGS.dir_cond_gen = Path(__file__).parent.parent / 'data/cond_gen'
+FLAGS.text_gen_lastlayer = 'softmax'
+
+FLAGS = set_paths(FLAGS)
+FLAGS.dir_clf = Path(os.getcwd()) / 'data/clfs/trained_classifiers_final'
+FLAGS.dir_gen_eval_fid = Path(os.getcwd()) / 'data/gen_eval_fid'
+FLAGS.use_clf = True
+FLAGS.batch_size = 30
+state_dict_path = experiment_path / 'checkpoints/0149/mm_vae'
+FLAGS.binary_labels = True
+mimic_experiment = MimicExperiment(flags=FLAGS)
+mimic_experiment.tb_logger = Dummylogger()
+mimic_experiment.mm_vae.to(FLAGS.device)
+mimic_experiment.mm_vae.load_state_dict(state_dict=torch.load(state_dict_path))
+mimic_experiment.mm_vae.eval()
+test_set = Mimic(FLAGS, mimic_experiment.labels, split='test')
+with torch.no_grad():
+    gen_eval = test_generation(0, mimic_experiment, dataset=test_set)
+results = flatten_cond_gen_values(gen_eval)
+print(results)
+
+out_path = Path(os.getcwd()) / 'data/gen_eval_results.json'
+log.info(f'Saving gen eval test results to {out_path}')
+with open(out_path, 'w') as outfile:
+    json.dump(results, outfile)
