9349843080fac3f75d4706dcdb1382b732951fd3
klugh <klugh@student.ethz.ch>
2021-06-05 15:45:20 +0200

    add lualatex compilation for poster

--- compile.sh
+++ compile.sh
@@ -2,24 +2,53 @@
 
 TARGET="${1}"
 WHITELIST="
+poster/midl2021_poster.tex
+"
+#  article.tex
+#  slides.tex
 
-  article.tex
-  slides.tex
-	"
+
 
 if [[ "$TARGET" = "all" ]] || [[ "$TARGET" == "" ]]; then
-	for ITER_TARGET in *.tex; do
-		if [[ $WHITELIST =~ (^|[[:space:]])$ITER_TARGET($|[[:space:]]) ]];then
-			ITER_TARGET=${ITER_TARGET%".tex"}
-			./compile.sh "${ITER_TARGET}"
-		fi
+	for ITER_TARGET in $WHITELIST; do
+
+    ITER_TARGET=${ITER_TARGET%".tex"}
+    ./compile.sh "${ITER_TARGET}"
+
 	done
+
 else
-	pdflatex -shell-escape "${TARGET}.tex" || { echo "Initial pdflatex failed"; exit $ERRCODE; }
-	pythontex.py "${TARGET}.tex" || { echo "PythonTeX failed"; exit $ERRCODE; }
-	pdflatex -shell-escape "${TARGET}.tex" || { echo "pdflatex failed after PythonTeX"; exit $ERRCODE; }
-	bibtex "${TARGET}" || { echo "bibtex failed"; exit $ERRCODE; }
-	pdflatex -shell-escape "${TARGET}.tex" || { echo "pdflatex failed after bibtex"; exit $ERRCODE; }
-	pdflatex -shell-escape "${TARGET}.tex" && python scripts/mv_article_pdf.py || exit 1
+  echo "${TARGET}"
+  if [ "${TARGET}" = "poster/midl2021_poster" ]; then
+#    the poster need to be run with lualatex.
+    lualatex -shell-escape "${TARGET}.tex" || { echo "Initial lualetex failed"; exit $ERRCODE; }
+    lualatex -shell-escape "${TARGET}.tex" || { echo "Second lualetex failed"; exit $ERRCODE; }
+
+  else
+    exit 1
+    pdflatex -shell-escape "${TARGET}.tex" || { echo "Initial pdflatex failed"; exit $ERRCODE; }
+
+    #  Only execute pythontex if indicated in latex file
+    first_line=$(head -n 1 "${TARGET}.tex")
+    if [ "${first_line}" = "% pythontex" ]; then
+        pythontex "${TARGET}.tex" || { echo "PythonTeX failed"; exit $ERRCODE; }
+        pdflatex -shell-escape "${TARGET}.tex" || { echo "pdflatex failed after PythonTeX"; exit $ERRCODE; }
+    fi
+
+    bibtex "$(basename "${TARGET}")" || { echo "bibtex failed"; exit $ERRCODE; }
+    pdflatex -shell-escape "${TARGET}.tex" || { echo "pdflatex failed after bibtex"; exit $ERRCODE; }
+    pdflatex -shell-escape "${TARGET}.tex"
+
+  fi
+
+
+  if [ ! -d tex_logs ]; then
+      mkdir tex_logs
+  fi
+	for CLEAN_TARGET in "*.aux *.log *.out *.bbl *.pytxcode *blx.bib *.blg *.run.xml *.bcf *.nav *.toc *.snm"; do
+	  mv $CLEAN_TARGET tex_logs/
+  done
+
+#	python scripts/mv_article_pdf.py || exit 1
 fi
 
